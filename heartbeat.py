30b713269336b624fcd83d7445872a03422ed5c9
s59338 <d-fly@gmx.net>
2017-09-26 10:50:06 +0200

    heartbeat

--- heartbeat.py
+++ heartbeat.py
@@ -2,21 +2,31 @@
 
 import serial, sys, time
 
+def heartbeat():
+	ser.write(B'\x42')
+
 def shutdown():
-    command = "/sbin/shutdown -h -t 30"
-    import subprocess
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    output = process.communicate()[0]
-    print(output)
+	command = "/sbin/shutdown -h -t sec 30"
+	import subprocess
+	process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+	output = process.communicate()[0]
+	print(output)
+	ser.flushOutput()
+	ser.write(B'\x108')
 
-def shutdown_abort():
-    command = "/sbin/shutdown -c"
-    import subprocess
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    output = process.communicate()[0]
-    print(output)
+def abort_shutdown():
+	command = "/sbin/shutdown -c"
+	import subprocess
+	process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+	output = process.communicate()[0]
+	print(output)
+	ser.flushOutput()
+	ser.write(B'\x109')
 
-serialPort = "/dev/ttyACM0"
+try:
+	serialPort = sys.argv[1]
+except:
+	serialPort = "/dev/ttyACM0"
 
 ser = serial.Serial(
     port=serialPort,
@@ -26,23 +36,26 @@ ser = serial.Serial(
     bytesize=serial.EIGHTBITS
 )
 
-ser.isOpen()
+mode = "heartbeat"
 
+commands = {	"heartbeat": heartbeat,
+		"shutdown": shutdown,
+		"abort shutdown": abort_shutdown,
+}
+
+millis = int(round(time.time() * 1000))
+
+ser.isOpen()
 while True:
-	print("Heartbeat")
-	ser.write(B'\x42')
-	bytesToRead = ser.inWaiting()
-	if (bytesToRead > 0):
-		cmd = ser.readline()
-		ser.flushInput()
-		ser.flushOutput()
-		print(cmd)
-		if (cmd == b'shutdown\r\n'):
-			shutdown()
-			ser.write(B'\x108')
-		if (cmd == b'abort\r\n'):
-			shutdown-abort()
-			ser.write(B'\x109')
-	time.sleep(3)
+	print(mode)
+	commands[mode]()
+	end_wait = int(round(time.time() * 1000)) + 2900
+	while (int(round(time.time() * 1000)) < end_wait):
+		bytesToRead = ser.inWaiting()
+		if (bytesToRead > 0):
+			cmd = str(ser.readline(), "utf-8")[:-2]
+			print(cmd)
+			ser.flushInput()
+			commands[cmd]()
 
 ser.close()
